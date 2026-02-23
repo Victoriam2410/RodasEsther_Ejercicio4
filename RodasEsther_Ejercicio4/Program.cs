573034fe2838646acff5af9b1e862762ecbd60d1
Victoriam2410 <estherrodas61@gmail.com>
2026-02-22 23:40:59 -0600

    Inicio del programa, declaracion de variables

--- RodasEsther_Ejercicio4/Program.cs
+++ RodasEsther_Ejercicio4/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+Console.WriteLine("Ingrese una de las opcciones: 1. Malware\r\n2. Phishing\r\n3. Acceso no autorizado\r\n4. Fuga de información");
+int incidente = int .Parse(Console.ReadLine());
+Console.WriteLine("Ingrese una de las opcciones: 1. PC\r\n2. Servidor\r\n3. Base de datos\r\n4. Red");
+int Activo = int .Parse(Console.ReadLine());
+Console.WriteLine("Persistencia detectada S/N");
+string persistencia = Console.ReadLine();
+Console.WriteLine("Numero de usuarios afectados");
+int usuarios = int.Parse(Console.ReadLine());
+string tipoIncidente = "";
+string tipoActivo = "";
+string severidad = "";
+string respuesta = "";
